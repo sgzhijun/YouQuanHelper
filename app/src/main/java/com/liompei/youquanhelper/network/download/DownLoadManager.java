0cb4e828eec0f4b3adaa98ee6cd2b19292876d60
liompei <blm1137694912@gmail.com>
2017-10-12 18:08:30 +0800

    添加下载功能

--- app/src/main/java/com/liompei/youquanhelper/network/download/DownLoadManager.java
+++ app/src/main/java/com/liompei/youquanhelper/network/download/DownLoadManager.java
@@ -5,7 +5,15 @@ import android.os.AsyncTask;
 import com.liompei.youquanhelper.network.APIFunction;
 import com.liompei.youquanhelper.network.config.HttpConfig;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -50,6 +58,9 @@ public class DownLoadManager {
             @Override
             protected Void doInBackground(Void... voids) {
                 Observable<ResponseBody> bodyCall = mAPIFunction.downloadPicture(pictureUrl);
+                     /*指定线程*/
+                bodyCall.subscribeOn(Schedulers.io());
+                bodyCall.unsubscribeOn(Schedulers.io());
 
                 return null;
             }
@@ -57,7 +68,46 @@ public class DownLoadManager {
 
     }
 
-    public void startDownload(){
+    public void writeCaches(ResponseBody responseBody, File file, DownInfo info) {
+        try {
+            RandomAccessFile randomAccessFile = null;
+            FileChannel channelOut = null;
+            InputStream inputStream = null;
+            try {
+                if (!file.getParentFile().exists())
+                    file.getParentFile().mkdirs();
+                long allLength = 0 == info.getCountLength() ? responseBody.contentLength() : info.getReadLength() + responseBody
+                        .contentLength();
+
+                inputStream = responseBody.byteStream();
+                randomAccessFile = new RandomAccessFile(file, "rwd");
+                channelOut = randomAccessFile.getChannel();
+                MappedByteBuffer mappedBuffer = channelOut.map(FileChannel.MapMode.READ_WRITE,
+                        info.getReadLength(), allLength - info.getReadLength());
+                byte[] buffer = new byte[1024 * 4];
+                int len;
+                while ((len = inputStream.read(buffer)) != -1) {
+                    mappedBuffer.put(buffer, 0, len);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (channelOut != null) {
+                    channelOut.close();
+                }
+                if (randomAccessFile != null) {
+                    randomAccessFile.close();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public void startDownload() {
 
     }
 
